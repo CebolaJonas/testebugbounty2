d541346ffa9d90b417e5f449482c53105c6f0057
CebolaJonas <105110332+CebolaJonas@users.noreply.github.com>
2024-08-28 16:21:24 +0000

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,26 @@
+<?php
+// URL do endpoint real
+$url = 'https://sales.apple.com/home';
+
+// Inicializar o cURL
+$ch = curl_init($url);
+
+// Configurações do cURL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // Recebe a resposta como string
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  // Segue redirecionamentos, se houver
+
+// Executa a solicitação e captura a resposta
+$response = curl_exec($ch);
+
+// Verifica se houve algum erro
+if (curl_errno($ch)) {
+    echo 'Erro: ' . curl_error($ch);
+} else {
+    // Retorna a resposta para o cliente
+    header("Content-Type: text/javascript");
+    echo $response;
+}
+
+// Fecha o cURL
+curl_close($ch);
+?>
