268e3cc686f9e8041c83a0735eebf514721ccc04
CebolaJonas <105110332+CebolaJonas@users.noreply.github.com>
2024-08-30 19:41:34 +0000

    Update protecteddata.js

--- api/protecteddata.js
+++ api/protecteddata.js
@@ -1,15 +1,6 @@
-const express = require('express');
 const axios = require('axios');
-const app = express();
-const cors = require('cors');
 
-// Permitir CORS para todos os domínios (ou ajuste conforme necessário)
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
-
-app.get('/api/protected-data', async (req, res) => {
+module.exports = async (req, res) => {
     try {
         // Captura os cookies enviados pelo navegador
         const cookies = req.headers.cookie;
@@ -22,10 +13,8 @@ app.get('/api/protected-data', async (req, res) => {
         });
 
         // Envia a resposta para o frontend
-        res.json(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
         res.status(500).send('Erro ao buscar dados');
     }
-});
-
-app.listen(3000, () => console.log('Servidor ouvindo na porta 3000'));
+};
